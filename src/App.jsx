c05b6a6fe80add74872f85c2f4b9cdd4c66036ca
madiha-ansari <157696198+madiha-ansari@users.noreply.github.com>
2025-01-26 08:47:03 +0500

    start the porfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit impedit hic, dignissimos explicabo sequi nostrum dolore atque sunt illo minima cumque exercitationem corrupti deserunt omnis molestiae voluptatem soluta quo odit.</h1>
+      <h1>hi</h1>
     </div>
   )
 }
